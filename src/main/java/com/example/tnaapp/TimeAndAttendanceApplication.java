c1732bbb80078275e0c8fa55060d68fe9ff0c711
Atsushi Hirosawa [X1] <hyakunen873@gmail.com>
2024-11-24 10:57:57 +0900

    This is my first commit.

---
+++ src/main/java/com/example/tnaapp/TimeAndAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tnaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAndAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAndAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tnaapp/TimeAndAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tnaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAndAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
