7b7a7b2fb582f0741cf8fb36ecab959dd3df60af
Atsushi Hirosawa N <hyakunen873@gmail.com>
2025-04-07 07:59:25 +0900

    最初のコミット

---
+++ src/main/java/com/example/tna_app/TimeAndAttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tna_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAndAttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAndAttendanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tna_app/TimeAndAttendanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tna_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAndAttendanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
