3bec7edc1f5576c10bd9b41a8169702e0d4e5f6b
Atsushi Hirosawa N <hyakunen873@gmail.com>
2025-04-10 07:48:00 +0900

    schema.sqlを追加

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,67 @@
+drop table if exists leave_request;
+drop table if exists change_request;
+drop table if exists timesheet;
+drop table if exists profile;
+drop table if exists account_role;
+drop table if exists account;
+drop table if exists role;
+
+-- アカウントテーブル
+CREATE TABLE IF NOT EXISTS account (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
+
+-- タイムシートテーブル
+CREATE TABLE IF NOT EXISTS timesheet (
+    account_id INTEGER NOT NULL,
+    working_day DATE,
+    working_status VARCHAR(20), -- 例: 'working', 'day off'...
+    attend_time TIME, --実際の出勤時間
+    leave_time TIME, --実際の退勤時間
+    overtime FLOAT,  --残業時間.出退勤から計算
+    stepout FLOAT, --中抜けに要した時間
+    finalized_flag TINYINT(1) NOT NULL, --確定フラグ。管理者への申請でtrue
+    edited_flag TINYINT(1) NOT NULL, --編集済みフラグ。編集後にtrue
+    requested_flag TINYINT(1) NOT NULL, --申請フラグ。申請中にtrue
+    PRIMARY KEY (account_id, working_day),
+    FOREIGN KEY (account_id) REFERENCES account(id)
+);
+
+-- プロフィールテーブル
+CREATE TABLE IF NOT EXISTS profile (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    age INTEGER,
+    sex VARCHAR(10),
+    address VARCHAR(255),
+    phone_number VARCHAR(),
+    joined_date DATE NOT NULL, -- 入社日
+    paid_dayoff INTEGER NOT NULL, -- 有給休暇の残日数
+    sub_dayoff INTEGER NOT NULL,  -- 振替休日の残日数
+    FOREIGN KEY (account_id) REFERENCES account(id)
+);
+
+-- 変更申請テーブル
+CREATE TABLE IF NOT EXISTS change_request (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    working_status VARCHAR(20),
+    attend_time TIME,
+    leave_time TIME,
+    reason VARCHAR(255),
+    apply_flag TINYINT(1),
+    FOREIGN KEY (timesheet_id) REFERENCES timesheet(id)
+);
+
+-- 休暇申請テーブル
+CREATE TABLE IF NOT EXISTS leave_request (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    working_day DATE NOT NULL
+    account_id INTEGER NOT NULL,
+    reason VARCHAR(255),
+    apply_flag TINYINT(1),
+ 	PRIMARY KEY (account_id, working_day),
+    FOREIGN KEY (account_id) REFERENCES account(id)
+); 
