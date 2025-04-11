e4b76b47e960a117eb28e68471b971aaf9692e9a
Atsushi Hirosawa N <hyakunen873@gmail.com>
2025-04-12 08:46:54 +0900

    data.sqlを追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO account (id, password, role) VALUES
+(1, '$2a$10$1MoxpigNI7IZFMX0zYrwb.Ia5MiUTb2yIOm9cMqJjOQfiFHW6VRgW', 'USER'),
+(2, '$2a$10$1MoxpigNI7IZFMX0zYrwb.Ia5MiUTb2yIOm9cMqJjOQfiFHW6VRgW', 'ADMIN');
