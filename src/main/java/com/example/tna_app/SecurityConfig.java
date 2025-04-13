05766dd50f8836caa8e0a3a498c3814fa2afb16e
Atsushi Hirosawa N <hyakunen873@gmail.com>
2025-04-13 22:39:25 +0900

    feat: ログイン機能を追加
    - Account.java アカウントのエンティティクラス
    - SecurityConfig.java セキュリティ設定クラス

---
+++ src/main/java/com/example/tna_app/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.tna_app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+        );
+        http.formLogin(form -> form
+                .defaultSuccessUrl("/home")
+        );
+
+        return http.build();
+    }
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+    }
+	
+	@Bean
+	UserDetailsManager users() {
+	    String userQuery = "select id,password,true from account where id = ?";
+	    String roleQuery = "select id,concat('ROLE_', role) from account where id = ?";
+	    
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.setUsersByUsernameQuery(userQuery);
+        users.setAuthoritiesByUsernameQuery(roleQuery);
+        
+        return users;
+	}
+}
---
+++ src/main/java/com/example/tna_app/entity/Account.java
@@ -0,0 +1,26 @@
+package com.example.tna_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="account")
+public class Account {
+	
+    @Id
+    @Column(name="id", nullable=false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="password", nullable=false)
+    private String password;
+    
+    @Column(name="role", nullable=false)
+    private String role;
+}
