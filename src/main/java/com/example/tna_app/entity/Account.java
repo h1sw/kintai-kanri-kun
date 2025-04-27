866a58c30061d1d63656c8d16def83e52daefda1
Atsushi Hirosawa N <hyakunen873@gmail.com>
2025-04-27 20:44:23 +0900

    fix: 不要なプロパティを削除

--- src/main/java/com/example/tna_app/entity/Account.java
+++ src/main/java/com/example/tna_app/entity/Account.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,12 +18,9 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="password", nullable=false)
+    @Column(name="password", nullable=false)	
     private String password;
     
     @Column(name="role", nullable=false)
     private String role;
-
-    @OneToOne
-    private Profile profile;
 }
