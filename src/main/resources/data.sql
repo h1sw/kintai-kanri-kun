232adebcfbd90c8dab82310ecf8308c5d5c25cca
Atsushi Hirosawa N <hyakunen873@gmail.com>
2025-04-28 07:49:02 +0900

    Chore: Profileテーブルのレコードを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO account (id, password, role) VALUES
 (1, '$2a$10$1MoxpigNI7IZFMX0zYrwb.Ia5MiUTb2yIOm9cMqJjOQfiFHW6VRgW', 'USER'),
 (2, '$2a$10$1MoxpigNI7IZFMX0zYrwb.Ia5MiUTb2yIOm9cMqJjOQfiFHW6VRgW', 'ADMIN');
+
+Insert into profile values
+(1, '利用者一郎', 25, '男', '千葉県船橋市西船橋1-2-3', '090-9999-9999', '2024-5-15', 10, 1),
+(2, '管理者次郎', 30, '男', '東京都大田区蒲田4-5-6', '090-9999-9999', '2022-6-30', 15, 0);
